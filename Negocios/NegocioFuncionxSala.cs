d6da64916da9f53dcdb0777b8e1300cdba2979f8
UNKNOWN-BADBOY <65840873+UNKNOWN-BADBOY@users.noreply.github.com>
2020-06-19 15:10:00 -0300

    Revert "Revert "Revert "Revert "-""""
    This reverts commit 3ef49807e2bcf37352275ebc01bd468cefbe4111.

---
+++ Negocios/NegocioFuncionxSala.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dao;
+using Entidades;
+
+namespace Negocios
+{
+    public class NegocioFuncionxSala
+    {
+        DaoFuncionxSala dao = new DaoFuncionxSala();
+
+        public SqlDataReader getFuncion()
+        {
+            return dao.cargar_funcion();
+        }
+
+        public SqlDataReader getFuncion_Sucursal(String valor)
+        {
+            return dao.cargar_funcion_sucursal(valor);
+        }
+
+    }
+}
